f5e0614aba270134817b9bbb8ec6ef2212fb81b2
jake-daniels <soltes.jakub@gmail.com>
2017-11-29 19:57:23 +0100

    Added basic source files and scripts for extension.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # zen-tab
+
+*Chrome extension. Replaces the default new tab page.*
+
+ **Zen Tab** is designed to help me organize my sh!t. No more, no less.
+
+Based on [React](https://facebook.github.io/react/) & [Redux](http://redux.js.org/).
+<br/>
+Created on top of [create-react-app](https://github.com/facebookincubator/create-react-app).
+<br/>
+
+## Installation guide
+[Basic packages](/docs/InstallationGuide.md)<br/>
---
+++ scripts/build-ext.js
@@ -0,0 +1,62 @@
+
+const chalk = require('chalk');
+const path = require('path');
+const fs = require('fs-extra');
+
+const APP_DIRECTORY = fs.realpathSync(process.cwd());
+const resolvePath = (relativePath) => {
+	return path.resolve(APP_DIRECTORY, relativePath);
+}
+
+const copy = (src, dest, filter) => {
+    const srcPath = resolvePath(src);
+    const destPath = resolvePath(dest);
+    fs.copySync(srcPath, destPath, {
+        clobber: true,
+        dereference: true,
+        preserveTimestamps: false,
+        filter: filter
+    });
+}
+
+console.log('-------------------------- [CHROME EXTENSION BUILD] --------------------------');
+console.log();
+
+console.log('  Creating directories...');
+
+fs.emptyDirSync(resolvePath('build-ext'));
+fs.mkdirsSync(resolvePath('build-ext/core'));
+fs.mkdirsSync(resolvePath('build-ext/resources'));
+
+console.log('  Copying files...');
+
+copy('extension', 'build-ext/resources', (file) => (file !== resolvePath('extension/manifest.json')));
+copy('extension/manifest.json', 'build-ext/manifest.json');
+copy('build', 'build-ext/core');
+
+console.log('  Updating index file...');
+
+const indexHtml = resolvePath('build-ext/core/index.html')
+fs.readFile(indexHtml, 'utf8', (err, data) => {
+
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+
+    const result = data.replace(/static/g, 'core/static').replace('/favicon', '/core/favicon');
+
+    fs.writeFile(indexHtml, result, 'utf8', (err) => {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+    });
+
+});
+
+console.log(chalk.yellow('  Extension built successfully.'));
+
+console.log();
+console.log('------------------------------------------------------------------------------');
+console.log();
